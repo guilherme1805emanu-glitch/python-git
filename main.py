bf003209f386888f1a7ce94d22fd466675265283
xorionx-lab <dennis.grupoaffectio@gmail.com>
2025-10-01 20:15:11 -0300

    Rota de testes

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def rota_inicial():
     return {"message": "Hello, World!"}
+
+@app.get("/teste")
+def rota_teste():
+    return {"message": "Rota de teste funcionando!"}
